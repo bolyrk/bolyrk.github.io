7a51accc2436ff0046cce452401f8fb113fdad77
bolyrk <patelmaryellen@gmail.com>
2025-04-18 17:21:39 +0800

    提交代码
    提交代码

--- js/mockApi.js
+++ js/mockApi.js
@@ -42,7 +42,7 @@ class MockApi {
                         name: "ChatGPT",
                         description: "强大的AI对话助手，解答问题、创作内容、辅助工作",
                         url: "https://chatgpt.com/",
-                        image: "img/1.jpg",
+                        image: "img/1.png",
                         sortOrder: 10
                     },
                     {
